85bf9c6cb33bc356497f46cc21ad8333b4602e32
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-07-12 00:46:30 +0530

    Created Default App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./components/home/Home"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+
   );
 }
 
